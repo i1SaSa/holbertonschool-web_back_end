37f9da848c3014a0a1bfb7b11e6cad7daf5755a9
i1SaSa <s9ny398335@gmail.com>
2026-04-15 19:25:25 +0300

    Fix indentation in docstring of sum_list function for consistency

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -4,10 +4,10 @@ from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
-    ''' 
+    '''
     take a list and return their sum
     @input_list: the list
-      '''
+    '''
     total = 0
     for i in input_list:
         total += i
