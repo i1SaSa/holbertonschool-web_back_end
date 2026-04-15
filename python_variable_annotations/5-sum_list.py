5c39f9d1bd162307a689db59a079af08ff2fe6f3
i1SaSa <s9ny398335@gmail.com>
2026-04-15 19:18:24 +0300

    Add sum_list function to calculate the summation of a list

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''module return summation of a list'''
+
+
+def sum_list(input_list: list) -> float:
+    ''' 
+    take a list and return their sum
+    @input_list: the list
+      '''
+    total = 0
+    for i in input_list:
+        total += i
+    return total
