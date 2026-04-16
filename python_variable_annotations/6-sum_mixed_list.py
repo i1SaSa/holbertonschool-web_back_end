28b6ce75548c1ffc1b885cf1939f47b27fbf6811
i1SaSa <s9ny398335@gmail.com>
2026-04-16 16:17:13 +0300

    Refactor sum_mixed_list function to support mixed list of int and float types

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python3
 '''module that return summation of list contian int, float'''
-from typing import List
+from typing import List, overload
 
 
-def sum_mixed_list(mxd_lst: List[float | int]) -> float:
+@overload
+def sum_mixed_list(mxd_lst: List[int]) -> float:
+    '''
+    func that return the summation of
+    '''
+    total = 0.0
+    for i in mxd_lst:
+        total += i
+    return total
+
+@overload
+def sum_mixed_list(mxd_lst: List[float]) -> float:
     '''
     func that return the summation of
     '''
