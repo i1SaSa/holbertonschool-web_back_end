637736d3a83631942b30a7994fe9b74acc5ab822
i1SaSa <s9ny398335@gmail.com>
2026-04-16 16:19:57 +0300

    Refactor sum_mixed_list function to simplify implementation and improve type handling

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,24 +1,8 @@
 #!/usr/bin/env python3
-'''module that return summation of list contian int, float'''
-from typing import List, overload
+""" module that return summation of list contian int, float """
+from typing import List, Union
 
 
-@overload
-def sum_mixed_list(mxd_lst: List[int]) -> float:
-    '''
-    func that return the summation of
-    '''
-    total = 0.0
-    for i in mxd_lst:
-        total += i
-    return total
-
-@overload
-def sum_mixed_list(mxd_lst: List[float]) -> float:
-    '''
-    func that return the summation of
-    '''
-    total = 0.0
-    for i in mxd_lst:
-        total += i
-    return total
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """ func that return the summation of a list of floats and ints """
+    return float(sum(mxd_lst))
