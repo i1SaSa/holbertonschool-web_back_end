e234349ea3d1801433429dc2f74c37dcb8ac2f87
i1SaSa <s9ny398335@gmail.com>
2026-04-17 17:05:41 +0300

    Update wait_random function to use random module directly and add type annotations

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 import asyncio
-from random import uniform
+import random
 
 
-async def wait_random(max_delay=10):
-    delay = uniform(0, max_delay)
+async def wait_random(max_delay: int = 10) -> float:
+    delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
