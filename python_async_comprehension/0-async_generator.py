7f9335b71b57af1c1150c7fbb0c4edafa88507aa
i1SaSa <s9ny398335@gmail.com>
2026-04-18 23:43:03 +0300

    Update docstrings in async_generator and async_comprehension for clarity

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-""" async generator"""
+"""Async generator module"""
 import asyncio
 import random
 from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
-    """func"""
+    """Generates 10 random numbers asynchronously"""
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-""" async comprehension"""
+"""Async comprehension module"""
 from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """async comprehension"""
+    """Collects 10 random numbers using async comprehension"""
     return [i async for i in async_generator()]
