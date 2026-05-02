40b8393df26f546f5c14d8748fe73292fc82d0e7
i1SaSa <s9ny398335@gmail.com>
2026-05-02 23:49:33 +0300

    Refactor log_stats script for improved clarity and consistency in Nginx log statistics

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Script that provides some stats about Nginx logs stored in MongoDB
+Log stats
 """
 from pymongo import MongoClient
 
@@ -9,19 +9,14 @@ if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
 
-    # Number of logs
-    total_logs = nginx_collection.count_documents({})
-    print(f"{total_logs} logs")
+    total_logs = nginx_collection.count()
+    print("{} logs".format(total_logs))
 
-    # Methods
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
-        count = nginx_collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
+        count = nginx_collection.count({"method": method})
+        print("\tmethod {}: {}".format(method, count))
 
-    # Status check
-    status_check = nginx_collection.count_documents(
-        {"method": "GET", "path": "/status"}
-    )
-    print(f"{status_check} status check")
+    status_check = nginx_collection.count({"method": "GET", "path": "/status"})
+    print("{} status check".format(status_check))
