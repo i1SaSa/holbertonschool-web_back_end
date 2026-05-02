fe2aea91e7a10606b89991d7d7d30639aa0f8651
i1SaSa <s9ny398335@gmail.com>
2026-05-02 22:17:31 +0300

    Implement deletion-resilient hypermedia pagination in Server class

---
+++ pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+import csv
+import math
+from typing import List, Dict
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """
+        Return a dictionary with resilient pagination information
+        """
+        data = self.indexed_dataset()
+        assert index is not None and index >= 0 and index < len(data)
+
+        result_data = []
+        current_index = index
+
+        # Loop to find the next available items starting from 'index'
+        while len(result_data) < page_size and current_index < len(data):
+            item = data.get(current_index)
+            if item:
+                result_data.append(item)
+            current_index += 1
+
+        next_index = current_index if current_index < len(data) else None
+
+        return {
+            'index': index,
+            'next_index': next_index,
+            'page_size': len(result_data),
+            'data': result_data
+        }
