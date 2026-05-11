38d68bd5b573bd55f50261114587981b3fdf9f80
i1SaSa <s9ny398335@gmail.com>
2026-05-11 16:57:45 +0300

    Add getStudentsByLocation function to filter students by city

---
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,11 @@
+export default function getStudentsByLocation(Student, city) {
+	if (typeof city !== 'string')
+		throw TypeError(`${city} Must be a String`)
+	if (!Array.isArray(Student)) {
+		return [];
+	}
+
+
+	return Student.filter(location)
+
+}
