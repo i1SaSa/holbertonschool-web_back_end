43061f557ef7310c9f0738d79d7416585e4a9d6c
i1SaSa <s9ny398335@gmail.com>
2026-05-20 17:23:48 +0300

    Fix syntax errors in 1-stdin.js by adding missing semicolons

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,8 +1,11 @@
-console.log("Welcome to Holberton School, what is your name?")
+console.log("Welcome to Holberton School, what is your name?");
 
 process.stdin.on('data', (data) => {
 	const name = data.toString().trim();
 	console.log(`Your name is: ${name}`);
-	console.log("This important software is now closing")
+});
+
+process.stdin.on('end', () => {
+	console.log("This important software is now closing");
 	process.exit();
 });
