99fbcf7f6d9dcff5de668b3a1190e289d2ba16e6
i1SaSa <s9ny398335@gmail.com>
2026-05-20 17:33:27 +0300

    Refactor output methods in 1-stdin.js to use process.stdout.write for consistency

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,14 @@
-console.log("Welcome to Holberton School, what is your name?");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
-	const name = data.toString().trim();
-	console.log(`Your name is: ${name}`);
+	process.stdout.write(`Your name is: ${data}`);
+	if (process.stdin.isTTY) {
+		process.stdout.write('\n');
+	}
 });
 
 process.stdin.on('end', () => {
-	console.log("This important software is now closing");
-	process.exit();
+	if (!process.stdin.isTTY) {
+		process.stdout.write('This important software is now closing\n');
+	}
 });
